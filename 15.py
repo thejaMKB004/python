6f718e6cab2fa068463d241c48d71be319c56c90
thejaMKB004 <thejabmk@gmail.com>
2025-11-27 14:29:27 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,5 @@
+color_list=["red","green","blue","white","black"]
+color_list2=["green","white","pink"]
+result=[color for color in color_list if color not in color_list2]
+print("colors in color_list not in color_list2:")
+print(result)
